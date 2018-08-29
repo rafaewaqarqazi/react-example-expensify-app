7e9e7b3c397a8759a33df19078f30d7503523f80
Rafae Waqar Qazi <rafaewaqar@gmail.com>
2018-08-29 12:48:07 +0500

    setup Production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,62 @@
 const path = require('path');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname,'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader:'babel-loader',
-            test:/\.js$/,
-            exclude:/node_modules/
-        },{
-            test:/\.s?css$/,
-            use:[
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool:'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname,'public'),
-        historyApiFallback:true
-    }
+module.exports = (env)=>{
+    const isProduction = env === 'production';
+    // const CSSExtract = new ExtractTextPlugin('styles.css');
+    return{
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname,'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader:'babel-loader',
+                test:/\.js$/,
+                exclude:/node_modules/
+            },{
+                test:/\.s?css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            // you can specify a publicPath here
+                            // by default it use publicPath in webpackOptions.output
+                            publicPath: '../'
+                        }
+                    },
+                    {
+                        loader: "css-loader",
+                        options:{
+                            sourceMap:true
+                        }
+                    },
+                    {
+                        loader: "sass-loader",
+                        options:{
+                            sourceMap:true
+                        }
+                    }
 
+
+                ]
+            }]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                // Options similar to the same options in webpackOptions.output
+                // both options are optional
+                filename: "styles.css",
+                chunkFilename: "[id].css"
+            })
+        ],
+        devtool: isProduction? 'source-map' :'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname,'public'),
+            historyApiFallback:true
+        }
+
+    };
 };
